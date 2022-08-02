e3ab3e507898f9b06abbfc632c77d235a80fa328
Sindre Bøyum <sindre.boyum@tietoevry.com>
2022-08-02 07:59:34 +0200

    H5P.newRunnable returns the initalized content type's instance

--- src/types/H5PObject.ts
+++ src/types/H5PObject.ts
@@ -2,6 +2,7 @@ import type { EventDispatcher } from "./EventDispatcher";
 import type { H5PConfirmationDialog } from "./H5PConfirmationDialog";
 import type { H5PCopyrightLicenses } from "./H5PCopyrightLicenses";
 import type { H5PDialog } from "./H5PDialog";
+import type { H5PExtras } from "./H5PExtras";
 import type { H5PMediaCopyright } from "./H5PMediaCopyright";
 import type { H5PMetadata } from "./H5PMetadata";
 import type { IH5PContentType } from "./IH5PContentType";
@@ -9,6 +10,9 @@ import type { IH5PContentType } from "./IH5PContentType";
 export type H5PObject = {
   // Constants
   copyrightLicenses: H5PCopyrightLicenses;
+
+  /** H5P content is rendered inside an iframe */
+  isFramed: boolean;
 
   // Functions
   buildMetadataCopyrights(metadata: H5PMetadata): H5PMediaCopyright;
@@ -18,10 +22,10 @@ export type H5PObject = {
   newRunnable: (
     library: { library: string; params: unknown },
     contentId: string,
-    $attachTo: JQuery,
+    $attachTo?: JQuery,
     skipResize?: boolean,
-    extras?: unknown
-  ) => void;
+    extras?: H5PExtras
+  ) => IH5PContentType;
 
   // Classes
   jQuery: typeof jQuery;
