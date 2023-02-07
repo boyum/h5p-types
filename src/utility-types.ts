4d48e152d0254f4a814e099483001fa4e9fc24c4
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-02-07 12:08:21 +0100

    chore: fix format

--- src/utility-types.ts
+++ src/utility-types.ts
@@ -7,5 +7,5 @@ export type DeepReadonly<T> = {
 
 export type Prettify<T> = {
   [P in keyof T]: T[P];
-} & // eslint-disable-next-line @typescript-eslint/ban-types
-{};
+  // eslint-disable-next-line @typescript-eslint/ban-types
+} & {};
