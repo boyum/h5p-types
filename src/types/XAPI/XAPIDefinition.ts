e43f5c4b248a758ccc23693396fe8bb5b7467a2f
Sindre Bøyum <boyum@users.noreply.github.com>
2023-04-21 08:07:24 +0200

    fix: improve XAPI related types (#177)

--- src/types/XAPI/XAPIDefinition.ts
+++ src/types/XAPI/XAPIDefinition.ts
@@ -1,13 +1,15 @@
-import type { XAPIInteractionType } from "./XAPIInteractionType";
-
 export type XAPIDefinition = {
   name: Record<string, string>;
   description: Record<string, string>;
   type: string;
-  interactionType: XAPIInteractionType;
 
   /**
    * @see https://github.com/adlnet/xAPI-Spec/blob/master/xAPI-Data.md#response-patterns
    */
-  correctResponsesPattern: string | Array<string>;
+  correctResponsesPattern?: string | Array<string>;
+  choices?: unknown;
+  scale?: unknown;
+  source?: unknown;
+  target?: unknown;
+  steps?: unknown;
 } & Record<string, unknown>;
