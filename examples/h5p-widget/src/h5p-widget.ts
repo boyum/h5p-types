88ece703c9034bc716b7a868ee4dab024220b93f
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-05-31 13:35:32 +0200

    test: add example projects for h5p content type and widget

---
+++ examples/h5p-widget/src/h5p-widget.ts
@@ -0,0 +1,37 @@
+import type { H5PFieldGroup, IH5PWidget } from "h5p-types";
+import { H5P, H5PWidget, registerWidget } from "h5p-utils";
+import library from "../library.json";
+
+type Field = H5PFieldGroup;
+type Params = {
+  name: string;
+  description: string;
+};
+
+// eslint-disable-next-line @typescript-eslint/no-empty-function, @typescript-eslint/no-unused-vars
+function arrayTest(_arr: Array<unknown>): void {}
+
+class Widget extends H5PWidget<Field, Params> implements IH5PWidget {
+  override appendTo($container: JQuery<HTMLElement>): void {
+    // `h5p-extensions.d.ts` extends `h5p-types-joubel-ui` which includes `H5P.JoubelUI`
+    const button = H5P.JoubelUI.createButton({
+      title: "",
+    });
+
+    // The field is a group
+    arrayTest(this.field.fields);
+
+    // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+    this.wrapper.append(button.get(0)!);
+    $container.append(this.wrapper);
+  }
+  override validate(): boolean {
+    return true;
+  }
+  override remove(): void {
+    this.wrapper.remove();
+  }
+}
+
+const contentTypeName = library.machineName.replace("H5P.", "");
+registerWidget("widget", contentTypeName, Widget);
