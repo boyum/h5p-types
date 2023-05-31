89a69cc53b379fb21bb752a24f7c776ae4c881aa
Sindre Bøyum <sindre.boyum@tietoevry.com>
2023-05-31 13:35:32 +0200

    test: add example projects for h5p content type and widget

---
+++ examples/h5p-widget/library.json.d.ts
@@ -0,0 +1,25 @@
+declare const json: {
+  "title": "Widget",
+  "machineName": "H5P.Widget",
+  "majorVersion": 1,
+  "minorVersion": 0,
+  "patchVersion": 0,
+  "runnable": 1,
+  "license": "MIT",
+  "author": "",
+  "embedTypes": ["iframe"],
+  "preloadedDependencies": [
+    {
+      "machineName": "H5P.JoubelUI",
+      "majorVersion": 1,
+      "minorVersion": 3
+    }
+  ],
+  "preloadedJs": [
+    {
+      "path": "dist/h5p-widget.js"
+    }
+  ]
+}
+
+export default json;
---
+++ examples/h5p-widget/webpack.config.js
@@ -0,0 +1,32 @@
+// @ts-check
+
+const path = require("path");
+
+const config = {
+  entry: {
+    "h5p-widget": path.join(__dirname, "src", "h5p-widget.ts"),
+  },
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [
+    // @ts-expect-error
+    require("unplugin-json-dts/webpack")(),
+  ],
+  resolve: {
+    modules: [path.resolve("./src"), path.resolve("./node_modules")],
+    extensions: [".ts", ".tsx", ".js", ".jsx", ".css"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts?$/,
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+module.exports = config;
