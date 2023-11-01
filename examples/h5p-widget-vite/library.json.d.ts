5603e4f5342f375808f0a019f755da86ce9ae6ae
Sindre Bøyum <boyum@users.noreply.github.com>
2023-11-01 15:48:36 +0100

    docs: add example showing how to build widgets with Vite (#510)

---
+++ examples/h5p-widget-vite/library.json.d.ts
@@ -0,0 +1,38 @@
+export const title : "Widget Vite";
+export const machineName : "H5P.WidgetVite";
+export const majorVersion : 1;
+export const minorVersion : 0;
+export const patchVersion : 0;
+export const runnable : 1;
+export const license : "MIT";
+export const author : "";
+export const embedTypes : [
+	"iframe"
+];
+export const preloadedDependencies : [
+	{
+		machineName: "H5P.JoubelUI",
+		majorVersion: 1,
+		minorVersion: 3
+	}
+];
+export const preloadedJs : [
+	{
+		path: "dist/h5p-widget-vite.js"
+	}
+];
+declare const $defaultExport: {
+	title: typeof title,
+	machineName: typeof machineName,
+	majorVersion: typeof majorVersion,
+	minorVersion: typeof minorVersion,
+	patchVersion: typeof patchVersion,
+	runnable: typeof runnable,
+	license: typeof license,
+	author: typeof author,
+	embedTypes: typeof embedTypes,
+	preloadedDependencies: typeof preloadedDependencies,
+	preloadedJs: typeof preloadedJs
+};
+
+export default $defaultExport;
---
+++ examples/h5p-widget-vite/vite.config.ts
@@ -0,0 +1,32 @@
+import jsonDts from "unplugin-json-dts/vite";
+import { defineConfig } from "vite";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  build: {
+    minify: "esbuild",
+
+    lib: {
+      entry: ["src/h5p-widget-vite.ts"],
+      formats: ["iife"],
+      name: "H5PWidgetVite",
+      fileName: () => "h5p-content-type-vite.js",
+    },
+
+    rollupOptions: {
+      output: {
+        assetFileNames: assetInfo => {
+          // For some reason, an H5P content type's style file cannot be named `style.css`.
+          // Therefore we need to change the name before saving it.
+          if (assetInfo.name === "style.css") {
+            return "h5p-widget-vite.css";
+          }
+
+          return assetInfo.name ?? "";
+        },
+      },
+    },
+  },
+
+  plugins: [jsonDts()],
+});
