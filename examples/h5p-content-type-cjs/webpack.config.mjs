c42c3db2984f692784247914174462e54f55b81d
Sindre Bøyum <boyum@users.noreply.github.com>
2023-12-29 18:42:03 +0100

    build(h5p-types-joubel-ui): cjs -> esm (#634)

---
+++ examples/h5p-content-type-cjs/webpack.config.mjs
@@ -0,0 +1,27 @@
+// @ts-check
+import { join, resolve } from "node:path";
+import jsonDts from "unplugin-json-dts/webpack";
+
+const __dirname = resolve();
+
+const config = {
+  entry: {
+    "h5p-content-type": join(__dirname, "src", "h5p-content-type.ts"),
+  },
+  output: {
+    path: resolve(__dirname, "dist"),
+    filename: "[name].js",
+  },
+  plugins: [jsonDts()],
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        use: [{ loader: "ts-loader" }],
+        exclude: /node_modules/,
+      },
+    ],
+  },
+};
+
+export default config;
--- packages/h5p-types-joubel-ui/test/index.test.ts
+++ packages/h5p-types-joubel-ui/test/index.test.ts
@@ -1,6 +1,6 @@
 /* eslint-disable @typescript-eslint/no-unused-vars, @typescript-eslint/ban-ts-comment, @typescript-eslint/no-namespace */
 import type { H5PObject } from "h5p-types";
-import type { AreEqual, Expect } from "./test-utility-types";
+import type { AreEqual, Expect } from "./test-utility-types.js";
 
 declare const H5P: H5PObject;
 H5P.$body;
