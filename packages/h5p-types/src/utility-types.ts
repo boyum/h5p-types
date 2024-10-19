2d0fb6a0f620dc287af5583f122e65e91b3b81a4
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-10-19 20:09:12 +0200

    feat(h5p-types): better widget safety

--- packages/h5p-types/src/utility-types.ts
+++ packages/h5p-types/src/utility-types.ts
@@ -3,3 +3,25 @@ export type Prettify<T> = {
 } & {};
 
 export type StrictOmit<T extends object, U extends keyof T> = Omit<T, U>;
+
+// Original source: https://github.com/typed-rocks/typescript/blob/main/one_of.ts
+type MergeTypes<TypesArray extends any[], Res = {}> = TypesArray extends [
+  infer Head,
+  ...infer Rem,
+]
+  ? MergeTypes<Rem, Res & Head>
+  : Res;
+
+export type OneOf<
+  TypesArray extends any[],
+  Res = never,
+  AllProperties = MergeTypes<TypesArray>,
+> = Prettify<
+  TypesArray extends [infer Head, ...infer Rem]
+    ? OneOf<Rem, Res | OnlyFirst<Head, AllProperties>, AllProperties>
+    : Res
+>;
+
+type SimpleOneOf<F, S> = OnlyFirst<F, S> | OnlyFirst<S, F>;
+
+type OnlyFirst<F, S> = F & { [Key in keyof Omit<S, keyof F>]?: never };
