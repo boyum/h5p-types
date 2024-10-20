42387f7ffcde8bf0a21d07cc033fc5f4296299ee
Sindre Bøyum <sindre.boyum@tietoevry.com>
2024-10-20 08:54:00 +0200

    build(h5p-generate-configs): esm-ify

---
+++ packages/h5p-generate-configs/bin/dev.js
@@ -0,0 +1,10 @@
+#!/usr/bin/env node
+
+(async () => {
+  const oclif = await import("@oclif/core");
+  await oclif.execute({
+    type: "esm",
+    dir: import.meta.url,
+    development: true,
+  });
+})();
---
+++ packages/h5p-generate-configs/bin/run.js
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+
+(async () => {
+  const oclif = await import("@oclif/core");
+  await oclif.execute({
+    type: "esm",
+    dir: import.meta.url,
+  });
+})();
