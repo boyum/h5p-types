a918640517aa594692ac6c951d2f9139dedabb0a
Sindre Bøyum <sindre.boyum@tietoevry.com>
2025-03-21 16:59:06 +0100

    chore: fix vite version to v5, as unplugin-json-dts doesn't work very well with v6

--- examples/h5p-content-type-vite/library.json.d.ts
+++ examples/h5p-content-type-vite/library.json.d.ts
@@ -7,22 +7,34 @@ export const patchVersion : 0;
 export const runnable : 1;
 export const license : "MIT";
 export const author : "";
-export const embedTypes : ["iframe"];
-export const preloadedDependencies : [{"machineName":"H5P.JoubelUI","majorVersion":1,"minorVersion":3}];
-export const preloadedJs : [{"path":"dist/h5p-content-type-vite.js"}];
+export const embedTypes : [
+	"iframe"
+];
+export const preloadedDependencies : [
+	{
+		machineName: "H5P.JoubelUI",
+		majorVersion: 1,
+		minorVersion: 3
+	}
+];
+export const preloadedJs : [
+	{
+		path: "dist/h5p-content-type-vite.js"
+	}
+];
 declare const $defaultExport: {
-  $schema,
-  title,
-  machineName,
-  majorVersion,
-  minorVersion,
-  patchVersion,
-  runnable,
-  license,
-  author,
-  embedTypes,
-  preloadedDependencies,
-  preloadedJs,
+	$schema: typeof $schema,
+	title: typeof title,
+	machineName: typeof machineName,
+	majorVersion: typeof majorVersion,
+	minorVersion: typeof minorVersion,
+	patchVersion: typeof patchVersion,
+	runnable: typeof runnable,
+	license: typeof license,
+	author: typeof author,
+	embedTypes: typeof embedTypes,
+	preloadedDependencies: typeof preloadedDependencies,
+	preloadedJs: typeof preloadedJs
 };
 
 export default $defaultExport;
