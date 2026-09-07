d698739697315fe24a8d0a79c05b72fd3e76ccfe
Sindre Bøyum <boyum@users.noreply.github.com>
2026-09-07 17:19:35 +0200

    build: update all dependencies

--- packages/h5p-types/test/EventDispatcher.test.ts
+++ packages/h5p-types/test/EventDispatcher.test.ts
@@ -7,7 +7,6 @@ import type { AreEqual, Expect } from "../src/test-utility-types";
 namespace Test_OnXAPIEvent {
   declare const contentType: EventDispatcher;
 
-  // biome-ignore lint/correctness/noUnusedFunctionParameters: Used for type testing
   contentType.on("xAPI", (event) => {
     type Actual = typeof event;
     type Expected = XAPIEvent;
@@ -23,7 +22,6 @@ namespace Test_OnXAPIEvent {
 namespace Test_OnAnyEvent {
   declare const contentType: EventDispatcher;
 
-  // biome-ignore lint/correctness/noUnusedFunctionParameters: Used for type testing
   contentType.on("anything-else", (event) => {
     type Actual = typeof event;
     type Expected = H5PEvent;
@@ -39,7 +37,6 @@ namespace Test_OnAnyEvent {
 namespace Test_OnceXAPIEvent {
   declare const contentType: EventDispatcher;
 
-  // biome-ignore lint/correctness/noUnusedFunctionParameters: Used for type testing
   contentType.once("xAPI", (event) => {
     type Actual = typeof event;
     type Expected = XAPIEvent;
@@ -55,7 +52,6 @@ namespace Test_OnceXAPIEvent {
 namespace Test_OnceAnyEvent {
   declare const contentType: EventDispatcher;
 
-  // biome-ignore lint/correctness/noUnusedFunctionParameters: Used for type testing
   contentType.once("anything-else", (event) => {
     type Actual = typeof event;
     type Expected = H5PEvent;
